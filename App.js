62f360a881be9a89b401fd7951ef64193dfd3ace
lucasgomescosta <lucasgomescosta@users.noreply.github.com>
2019-11-12 15:21:19 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,61 +1,133 @@
 import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity
+  } from 'react-native';
 
-import { View, TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
+export default class App extends Component{
 
-// import { Container } from './styles';
+  constructor(props){
+    super(props);
+    this.state = {
+      numero: 0,
+      botao: 'VAI'
+    };
 
-export default class App extends Component {
+    //variavel do timer do relogio
+    this.timer = null;
 
-  render() {
-    return (
-      <View style={styles.container}>
-          <Image source={require('./src/cronometro.png')} />
-          <Text style={styles.time}>0</Text>
-            <View style={styles.btnarea}>
-              <TouchableOpacity style={styles.botao}>
-                <Text style={styles.btntexto}> Vai</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.botao}>
-                <Text style={styles.btntexto}>Limpar</Text>
-              </TouchableOpacity>
-            </View>
-      </View>
-    )
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
   }
+
+  vai(){
+    let state = this.state;
+
+    if(this.timer != null){
+
+      //Aqui vai parar o timer
+      clearInterval(this.timer);
+      this.timer = null;
+
+      state.botao = "VAI";
+
+    }else{
+
+      // Comeca girar o timer!
+      this.timer = setInterval(()=>{
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      },100);
+
+      state.botao = "PARAR";
+
+    }
+
+    this.setState(state);
+
+  }
+
+  limpar(){
+
+    if(this.timer != null){
+
+      //Aqui vai parar o timer
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+
+    let state = this.state;
+    state.numero = 0;
+    state.botao = "VAI";
+    this.setState(state);
+
+  }
+
+  render(){
+
+    return(
+      <View style={styles.container} > 
+
+        <Image source={require('./src/cronometro.png')} />
+        <Text style={styles.timer} >{this.state.numero.toFixed(1)}</Text>
+
+        <View style={styles.btnArea}>
+
+          <TouchableOpacity style={styles.botao} onPress={this.vai} >
+             <Text style={styles.btnTexto}>{this.state.botao}</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao} onPress={this.limpar} >
+             <Text style={styles.btnTexto}>LIMPAR</Text>
+          </TouchableOpacity>
+
+        </View>
+
+      </View>    
+    );
+
+  }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 30,
+  container:{
+    flex:1,
+    paddingTop: 20,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#00aeef'
   },
-  time: {
+  timer:{
     marginTop: -160,
-    color: '#FFFF',
+    color: '#FFFFFF',
     fontSize: 72,
     fontWeight: 'bold',
   },
-  btnarea: {
+  btnArea:{
     flexDirection: 'row',
     marginTop: 70,
     height: 40
   },
-  botao: {
+  botao:{
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFFF',
+    backgroundColor: '#ffffff',
     height: 40,
     margin: 17,
     borderRadius: 9
   },
-  btntexto: {
+  btnTexto:{
     fontSize: 20,
     fontWeight: 'bold',
     color: '#00aeef'
   }
+ 
 
 });
+
